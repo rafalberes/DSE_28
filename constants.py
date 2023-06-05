7b721dff33484451031d3f782d42387958518def
Tim Dammerman <99322410+TDammerman@users.noreply.github.com>
2023-06-05 10:20:57 +0200

    Calculation of DeltaO added with needed constants

--- constants.py
+++ constants.py
@@ -3,3 +3,6 @@ J_2 = 1.08262668e-3     # [-] https://ai-solutions.com/_freeflyeruniversityguide
 R_E = 6378.137e3        # [m]
 mu_E = 3.986004418e14   # [m^3/s^2]
 T_E = 23 * 60 * 60 + 56 * 60 + 4.0916    # [s] universe today <- source
+
+## Conversions
+m_to_deg = 1 / 111_139  # Conversion to find longitude/latitude degrees from surface distance https://sciencing.com/convert-latitude-longtitude-feet-2724.html
--- satellite.py
+++ satellite.py
@@ -8,15 +8,17 @@ class Satellite:
 	def __init__(self, i=0, e=0,
 	             FoV_hor=np.deg2rad(20), FoV_ver=np.deg2rad(10), mass=1000, frontal_area=2,
 	             Res_spac=10, Res_temp=1):
+		
 		self.T = None  # Orbital period [s]
 		self.r_p = None  # Peri-centre radius [m]
 		self.e = e  # Eccentricity [-]
 		self.a = None  # Semi-major axis [m]
 		self.r_a = None  # Apo-centre radius [m]
 		self.i = i  # Inclination angle [Rad]
-		self.DeltaL1 = None
-		self.DeltaL2 = None
-		self.DeltaL = None
+		self.DeltaL1 = None  # Shift in longitude per obit due to rotation of the Earth
+		self.DeltaL2 = None  # Shift in longitude per orbit due to the J2 effect [rad]
+		self.DeltaL = None  # Total shift in longitude per orbit [rad]
+		self.DeltaO = None  # Spacing in longitude needed of orbits within one earth-repeat orbit [rad]
 		
 		## Payload characteristics
 		self.FoV_hor = FoV_hor  # Field of view in horizontal direction [Rad]
@@ -27,6 +29,8 @@ class Satellite:
 		## Mission Requirements
 		self.Res_spac = Res_spac  # Spacial resolution [m/pixel]
 		self.Res_temp = Res_temp  # Temporal resolution [/hour]
+		self.width_ver = None  # Latitude distance able to observe at a time [m]
+		self.width_hor = None  # Longitude distance able to observe at a time [m]
 		
 		self.mass = mass  # Satellite mass [kg]
 		self.frontal_area = frontal_area  # Frontal area [m^2]
@@ -34,7 +38,7 @@ class Satellite:
 		self.calc_orbital_parameters()
 	
 	def calc_orbital_parameters(self):
-		self.calc_rp_from_res_spac()
+		self.calc_rp_and_DeltaO()
 		
 		self.a = self.r_p / (1 - self.e)
 		self.r_a = self.a * (1 + self.e)
@@ -45,19 +49,24 @@ class Satellite:
 		self.DeltaL2 = (- 3 * np.pi * const.J_2 * const.R_E ** 2 * np.cos(self.i)) / (self.a ** 2 * (1 - self.e ** 2) ** 2)
 		self.DeltaL = self.DeltaL1 + self.DeltaL2
 		
-	def calc_rp_from_res_spac(self):
+	def calc_rp_and_DeltaO(self):
 		## Horizontal
-		width_hor = self.Res_spac * self.PL_pix_hor
-		r_p_altitude_hor = 1 / 2 * width_hor / (np.tan(1 / 2 * self.FoV_hor))
+		self.width_hor = self.Res_spac * self.PL_pix_hor
+		r_p_altitude_hor = 1 / 2 * self.width_hor / (np.tan(1 / 2 * self.FoV_hor))
 		
 		## Horizontal
-		width_ver = self.Res_spac * self.PL_pix_ver
-		r_p_altitude_ver = 1 / 2 * width_ver / (np.tan(1 / 2 * self.FoV_ver))
+		self.width_ver = self.Res_spac * self.PL_pix_ver
+		r_p_altitude_ver = 1 / 2 * self.width_ver / (np.tan(1 / 2 * self.FoV_ver))
 		
-		self.r_p = min(r_p_altitude_hor, r_p_altitude_ver) + const.R_E
-	
-	def calc_DeltaO(self):
-		pass
+		r_p_altitude = min(r_p_altitude_hor, r_p_altitude_ver)
+		print(f"Altitude r_p: {r_p_altitude * 10 ** -3} [km]")
+		
+		if r_p_altitude < 160e3:
+			print("Equipment not sufficient")
+			quit()
+		else:
+			self.r_p = r_p_altitude + const.R_E
+			self.DeltaO = np.deg2rad(r_p_altitude * const.m_to_deg)
 	
 	def calc_DeltaL(self):
 		pass
@@ -86,6 +95,7 @@ class Satellite:
 		
 		if e < 0 or e >= 1:
 			print("Not realistic e:", e)
+			quit()
 		else:
 			self.e = e
 			self.calc_orbital_parameters()
